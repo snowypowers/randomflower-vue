8a3c04d508f100a1b10b75014eaa2acc23e684f0
Yak Jun Xiang <snowypowers@gmail.com>
2017-03-03 19:24:02 +0800

    Use Roboto font to be constant with VueMaterial
    remove top margin

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
   name: 'App',
   data () {
     return {
-      message: "Hello World"
+
     }
   }
 }
@@ -23,12 +23,11 @@ export default {
 
 <style lang="stylus">
 #app
-  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  font-family: Roboto, Helvetica, Arial, sans-serif
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
   text-align: center
   color: #2c3e50
-  margin-top: 60px
   border-style: solid
 
 #splash
