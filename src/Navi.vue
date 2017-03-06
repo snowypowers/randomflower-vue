e24ac9804bd4f816dbc12fffcc78bdd79afb68f2
Yak Jun Xiang <snowypowers@gmail.com>
2017-03-06 15:47:35 +0800

    Add sticky function to Navi

--- src/Navi.vue
+++ src/Navi.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
-div
-    md-toolbar#navi
+div(v-bind:class="{stickTop: isSticky}")
+    md-toolbar
         md-avatar.md-primary
             md-icon favorite border
         h2.md-title.md-hide-medium-and-up RF
@@ -14,14 +14,35 @@ export default {
   name: 'Navi',
   data () {
     return {
+        offsetY:0,
+        isSticky: false
     }
+  },
+  methods: {
+      calculateSticky: function(e) {
+          let currentY = window.scrollY
+          this.isSticky = currentY > this.offsetY? true : false
+      }
+  },
+  mounted() {
+    this.offsetY = this.$el.offsetTop
+  },
+  created() {
+      
+      window.addEventListener('scroll', this.calculateSticky)
+  },
+  destroyed() {
+      window.removeEventListener('scroll', this.calculateSticky)
   }
 }
 </script>
 
 <style lang="stylus" scoped>
+.stickTop
+    position: fixed
+    top: 0px
+
 #navi
-    position: relative
     width: 100%
     z-index: 999
 
@@ -29,9 +50,6 @@ h2
     flex: 1
     text-align: left
 
-.toTop
-    position: fixed
-    top: 0px
 
 
 </style>
