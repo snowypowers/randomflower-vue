6be45972c8fde6bdcf784f13c3e6c4bac12cab39
Yak Jun Xiang <snowypowers@gmail.com>
2017-03-10 01:38:19 +0800

    Add Decks header for deckfield with add deck button

--- src/DeckField.vue
+++ src/DeckField.vue
@@ -1,5 +1,11 @@
 <template lang="pug">
 div.container-fluid.no-margin
+  md-toolbar.md-accent#deckhead
+    h4.md-title(style="flex: 1; text-align: left;") Decks
+    md-button.md-icon-button
+      md-icon add_circle
+    md-button.md-icon-button
+      md-icon remove_circle
   DeckCard(v-for="(deck, index) in decks" v-bind:key="index" v-bind:deckName="deck.deckName" v-bind:deckClass="deck.deckClass" v-bind:deckNum="index")
 
 </template>
